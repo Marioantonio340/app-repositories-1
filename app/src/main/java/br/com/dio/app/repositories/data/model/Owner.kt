0ac78512c6e70a43a1c7d9e04fca0b16f93fe770
Marioantonio340 <marioantonio340@gmail.com>
2021-10-14 15:25:09 -0300

    Aula 6 - Finalização do app

--- app/src/main/java/br/com/dio/app/repositories/data/model/Owner.kt
+++ app/src/main/java/br/com/dio/app/repositories/data/model/Owner.kt
@@ -1,9 +1,12 @@
 package br.com.dio.app.repositories.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Owner (
     val login: String,
     val id: Long,
     val nodeID: String,
+    @SerializedName("avatar_url")
     val avatarURL: String,
     val gravatarID: String,
     val url: String,
--- app/src/main/java/br/com/dio/app/repositories/data/model/Repo.kt
+++ app/src/main/java/br/com/dio/app/repositories/data/model/Repo.kt
@@ -1,12 +1,19 @@
 package br.com.dio.app.repositories.data.model
 
+import com.google.gson.annotations.SerializedName
 
 
 data class Repo (
     val id: Long,
     val nodeID: String,
     val name: String,
+    val description: String,
+    @SerializedName("stargazers_count")
+    val stargazersCount:Long,
     val fullName: String,
+    val language: String,
+    @SerializedName("html_url")
+    val htmlUrl:String,
     val private: Boolean,
     val owner: Owner
 )
